df4f4422d3fe9254605c95fb324a56d686a1ac2c
Nemo <nemo@debian>
2024-06-03 19:31:05 +0200

    Initial tunning green/brown edge detection

--- src/feature_detection.py
+++ src/feature_detection.py
@@ -12,18 +12,20 @@ def table_edges_detection(frame_BGR: MatLike) -> MatLike:
 
     frame_HSV = cv2.cvtColor(frame_BGR, cv2.COLOR_BGR2HSV)
 
+	#CV2 HSV ranges: Hue from 0 to 180, Staturation form 0 to 100, Value from 0 to 100
+
     # Filter green color
-    green_HSV_low = np.array([50, 200, 110])  # Hardcoded green HSV value
-    green_HSV_high = np.array([70, 255, 210])
+    green_HSV_low = np.array([50, 200, 50])  # Hardcoded green HSV value
+    green_HSV_high = np.array([70, 255, 220])
     frame_green = cv2.inRange(frame_HSV, green_HSV_low, green_HSV_high)
 
     # Filter brown color
-    brown_HSV_low = np.array([0, 70, 35])  # Hardcoded brown HSV value
-    brown_HSV_high = np.array([40, 210, 130])
+    brown_HSV_low = np.array([0, 0, 20])  # Hardcoded brown HSV value
+    brown_HSV_high = np.array([20, 200, 150])
     frame_brown_1 = cv2.inRange(frame_HSV, brown_HSV_low, brown_HSV_high)
 
-    brown_HSV_low = np.array([150, 70, 35])  # Other brown
-    brown_HSV_high = np.array([255, 210, 130])
+    brown_HSV_low = np.array([120, 0, 20])  # Other brown
+    brown_HSV_high = np.array([180, 200, 150])
     frame_brown_2 = cv2.inRange(frame_HSV, brown_HSV_low, brown_HSV_high)
 
     frame_brown = cv2.bitwise_or(frame_brown_1, frame_brown_2)
@@ -32,17 +34,29 @@ def table_edges_detection(frame_BGR: MatLike) -> MatLike:
     # frame_green = cv2.morphologyEx(frame_green, cv2.MORPH_OPEN, kernel)
     # frame_green = cv2.morphologyEx(frame_green, cv2.MORPH_CLOSE, kernel)
 
-    # Edge detection between green and brown
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    green_dilate = cv2.dilate(frame_green, kernel)
-    brown_dilate = cv2.dilate(frame_brown, kernel)
-    green_brown_edges = cv2.bitwise_and(green_dilate, brown_dilate)
-
-    # Edge detection using morphological gradient
-    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2,2))
-    # frame_brown = cv2.morphologyEx(frame_brown, cv2.MORPH_GRADIENT, kernel)
-
-    lines = hough_lines(green_brown_edges, num_lines=4)
+    # Dilate green and brown so that they overlap
+    kernel_small = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10)) # Hardcodec value
+    brown_dilate = cv2.dilate(frame_brown, kernel_small)
+    green_dilate = cv2.dilate(frame_green, kernel_small)
+    
+    # Remove some balls from the green playing area (red balls are too big of a blob)
+    kernel_ball_size = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20)) # Hardcodec value
+    green_close = cv2.morphologyEx(green_dilate, cv2.MORPH_CLOSE, kernel_ball_size)
+    
+    # Get a thin edge of the green playing area
+    kernel_very_small = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2)) # Hardcodec value
+    green_gradient = cv2.morphologyEx(green_close, cv2.MORPH_GRADIENT, kernel_very_small)
+    
+    # Intersect the greeen edge with the brown mask to obtain green/brow edges
+    edges_green_brown = cv2.bitwise_and(green_gradient, brown_dilate)
+    
+    # Draw edges in white
+    #edges_negative = cv2.bitwise_not(edges_green_brown)
+    #frame_BGR = cv2.bitwise_and(frame_BGR,frame_BGR, mask=edges_negative)
+    #frame_BGR = frame_BGR + cv2.cvtColor(edges_green_brown, cv2.COLOR_GRAY2RGB)
+	
+	# Compute lines that fit the edges
+    lines = hough_lines(edges_green_brown, num_lines=4)
 
     # Draw lines from Hough transform
     if lines is not None:
@@ -53,17 +67,13 @@ def table_edges_detection(frame_BGR: MatLike) -> MatLike:
             b = np.sin(theta)
             x0 = a * rho
             y0 = b * rho
-            x1 = int(x0 + 1000 * (-b))
-            y1 = int(y0 + 1000 * (a))
-            x2 = int(x0 - 1000 * (-b))
-            y2 = int(y0 - 1000 * (a))
+            x1 = int(x0 + 10000 * (-b))
+            y1 = int(y0 + 10000 * (a))
+            x2 = int(x0 - 10000 * (-b))
+            y2 = int(y0 - 10000 * (a))
 
-            cv2.line(frame_BGR, (x1, y1), (x2, y2), (0, 0, 255), 1)
+            cv2.line(frame_BGR, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-    # frame -> frame_BGR
-    # frame[:,:,0] = np.zeros((width,height))
-    # frame[:,:,1] = frame_brown
-    # frame[:,:,1] = cv2.bitwise_or(frame[:,:,1],green_brown_edges)
     return frame_BGR
 
 
